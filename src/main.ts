74003b4572cd145248431b017d1b52b163776b82
sangddong <hehe74029@gmail.com>
2024-09-23 22:12:38 +0900

    feat: set validation pipe, excepttion filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   app.listen(configService.get('PORT') || 3000);
 }
