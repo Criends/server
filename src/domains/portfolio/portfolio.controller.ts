bf9ca304ef9ec2fa5058ad4dacfdf501d672e956
sangddong <hehe74029@gmail.com>
2024-10-07 17:23:45 +0900

    feat: 포트폴리오 단일/리스트 호출 구현

--- src/domains/portfolio/portfolio.controller.ts
+++ src/domains/portfolio/portfolio.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { PortfolioService } from './portfolio.service';
 import { Guard } from 'src/decorators/guard.decorator';
+import { DGetAllResumes } from '../resume/resume.dto';
 
 @Controller('portfolio')
 export class PortfolioController {
   constructor(private readonly portfolioService: PortfolioService) {}
+
+  @Get()
+  async findAllPortfolio(@Body() data: DGetAllResumes) {
+    return await this.portfolioService.getAllPortfolio(data);
+  }
 
   @Guard(['user', 'company'])
   @Get(':id')
--- src/domains/portfolio/portfolio.service.ts
+++ src/domains/portfolio/portfolio.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { DGetAllResumes, SortResume } from '../resume/resume.dto';
 
 @Injectable()
 export class PortfolioService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async getAllPortfolio(data: DGetAllResumes) {
+    const orderByField: object[] = [];
+
+    switch (data.sort) {
+      case SortResume.UPDATED_AT:
+        orderByField.push({ updatedAt: 'desc' });
+        break;
+      case SortResume.LIKES:
+        orderByField.push({ likes: 'desc' }, { updatedAt: 'desc' });
+        break;
+      case SortResume.PROPOSAL:
+        orderByField.push({ proposal: 'desc' }, { updatedAt: 'desc' });
+        break;
+    }
+
+    return await this.prismaService.portfolio.findMany({
+      where: { expose: data.expose },
+      orderBy: orderByField,
+    });
+  }
 
   async getPortfolio(portfolioId: string) {
     return await this.prismaService.portfolio.findUnique({
